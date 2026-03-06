edf0a57577de4ff868d19bde9c5501bba9913e2e
nipanzuzuzu9 <nipanzuzuzu9@gmail.com>
2026-03-06 04:28:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -129,4 +129,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
